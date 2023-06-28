83ecedc3f9f28b3e5926590c3b2149a41af2c3e7
Iosif Rodopoulos <bonkeyzz10@gmail.com>
2023-06-28 04:18:08 +0300

    Minor code optimizations.

--- matcher.py
+++ matcher.py
@@ -3,20 +3,17 @@ from os import walk, remove
 import json
 from PIL import Image
 
-def match_with_features(img1, img2):
+def match_images(img1, img2, sort=True):
 	orb = cv2.ORB_create(nfeatures=500)
 	kp1, des1 = orb.detectAndCompute(img1, None)
 	kp2, des2 = orb.detectAndCompute(img2, None)
 
 	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 	matches = bf.match(des1, des2)
-
-	return len(matches)
+	if sort: matches = sorted(matches, key=lambda x: x.distance)
+	return kp1, kp2, matches
 
 def match_features_and_draw(img1, img2, name, result, sort=True):
-	orb = cv2.ORB_create(nfeatures=500)
-	kp1, des1 = orb.detectAndCompute(img1, None)
-	kp2, des2 = orb.detectAndCompute(img2, None)
 	# The font to display this in (hate it)
 	font = cv2.FONT_HERSHEY_DUPLEX
 	# Org - Position of text
@@ -27,9 +24,7 @@ def match_features_and_draw(img1, img2, name, result, sort=True):
 	color = (255, 255, 5)
 	thickness = 2
 
-	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-	matches = bf.match(des1, des2)
-	if sort: matches = sorted(matches, key=lambda x: x.distance)
+	kp1, kp2, matches = match_images(img1, img2, True)
 	img2 = cv2.putText(img2, 'Source', org, font, fontScale, color, thickness, cv2.LINE_AA)
 	img1 = cv2.putText(img1, 'Template', org, font, fontScale, color, thickness, cv2.LINE_AA)
 	match_img = cv2.drawMatches(img1, kp1, img2, kp2, matches[:50], None)
@@ -50,18 +45,18 @@ template_list = []
 
 #NOTE - You can change this to any test spawns you want.
 #TODO - Integrate into discord as a self-bot application.
-poketwo_spawn = Cv2Image('./test_poketwo_spawns/poketwo_deino.png')
+poketwo_spawn = Cv2Image('./test_poketwo_spawns/poketwo_esp.png')
 
 def check_matching(src, templ): 
 	#FIXME - Matching may fail if picture is flipped
 	#FIXME - Completely different pokemon may still be matched as there's no way (at the moment) to determine if we have template image of a pokemon for the source image (poketwo)
 	#match_len = match_with_features(src, templ)
 	templ_flipped = cv2.flip(templ, 1)
-	match_len_flipped = match_with_features(src, templ_flipped)
+	kp1, kp2, matches = match_images(src, templ_flipped, False)
 
 	#if match_len_flipped >= match_len: return match_len_flipped
 	#else: 
-	return match_len_flipped
+	return len(matches)
 
 def jpg2png(input: str):
 	im1 = Image.open(input, 'r')
